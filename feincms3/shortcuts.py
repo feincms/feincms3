a4337138ea96a189568a945a53ae6dff59e39201
Matthias Kestenholz <mk@feinheit.ch>
2016-06-23 11:57:37 +0200

    Not later, now.

--- feincms3/shortcuts.py
+++ feincms3/shortcuts.py
@@ -1,7 +1,7 @@
 """
 For me, the most useful part of Django's generic class based views is
 the template name generation and the context variable naming for list
-and detail views, and maybe later also the pagination.
+and detail views, and also the pagination.
 
 The rest of the CBV is less flexible than I'd like them to be, i.e.
 integrating forms on detail pages can be a hassle.
