ed2fd33e7ae670be65e36bfc63dc4a4c15653e84
Matthias Kestenholz <mk@feinheit.ch>
2016-07-02 12:53:13 +0200

    Test adding pages, test the richtext plugin a bit

---
+++ tests/testapp/admin.py
@@ -0,0 +1,81 @@
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from django.utils.text import capfirst
+from django.utils.translation import ugettext_lazy as _
+
+from content_editor.admin import ContentEditor
+
+from mptt.admin import DraggableMPTTAdmin
+
+from feincms3 import plugins
+
+from . import models
+
+
+@admin.register(models.Page)
+class PageAdmin(DraggableMPTTAdmin, ContentEditor):
+    list_display = [
+        'tree_actions', 'indented_title',
+        'is_active', 'menu',
+        'language_code',
+        'template_key',
+        'application',
+    ]
+    list_filter = ['is_active', 'menu']
+    list_editable = ['is_active']
+    list_display_links = ['indented_title']
+    prepopulated_fields = {
+        'slug': ('title',),
+    }
+    radio_fields = {
+        'menu': admin.HORIZONTAL,
+        'language_code': admin.HORIZONTAL,
+        'template_key': admin.HORIZONTAL,
+        'application': admin.HORIZONTAL,
+    }
+    raw_id_fields = ['parent']
+
+    mptt_level_indent = 30
+
+    fieldsets = [
+        (None, {
+            'fields': (
+                'is_active',
+                'title',
+                'parent',
+            )
+        }),
+        (capfirst(_('path')), {
+            'fields': (
+                'slug',
+                'static_path',
+                'path',
+            ),
+            'classes': ('tabbed',),
+        }),
+        (capfirst(_('settings')), {
+            'fields': (
+                'menu',
+                'language_code',
+                'template_key',
+                'application',
+            ),
+            'classes': ('tabbed',),
+        }),
+    ]
+
+    inlines = [
+        plugins.RichTextInline.create(model=models.RichText),
+        plugins.ImageInline.create(model=models.Image),
+        plugins.SnippetInline.create(model=models.Snippet),
+        plugins.ExternalInline.create(model=models.External),
+    ]
+
+    class Media:
+        css = {'all': (
+            'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',  # noqa
+        )}
+        js = (
+            'app/plugin_buttons.js',
+        )
--- tests/testapp/test_feincms3.py
+++ tests/testapp/test_feincms3.py
@@ -1,21 +1,162 @@
-import re
-import time
+from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
-from django.core import mail
-from django.core.urlresolvers import reverse
-from django.test import TestCase
-from django.test.client import RequestFactory
-from django.utils import timezone
-from django.utils.http import urlunquote
+from django.test import Client, TestCase
 
-# from email_registration.utils import get_signer, send_registration_mail
+from .models import Page
 
 
 def _messages(response):
     return [m.message for m in response.context['messages']]
 
 
-class TestTest(TestCase):
-    def test_setup(self):
-        pass
+def zero_management_form_data(prefix):
+    return {
+        '%s-TOTAL_FORMS' % prefix: 0,
+        '%s-INITIAL_FORMS' % prefix: 0,
+        '%s-MIN_NUM_FORMS' % prefix: 0,
+        '%s-MAX_NUM_FORMS' % prefix: 1000,
+    }
+
+
+def merge_dicts(*dicts):
+    res = {}
+    for d in dicts:
+        res.update(d)
+    return res
+
+
+class AdminTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            'admin', 'admin@test.ch', 'blabla')
+
+    def login(self):
+        client = Client()
+        client.force_login(self.user)
+        return client
+
+    def test_modules(self):
+        client = self.login()
+
+        response = client.get('/admin/')
+        self.assertContains(
+            response,
+            '<a href="/admin/testapp/page/">Pages</a>',
+            1,
+        )
+
+        response = client.get('/admin/testapp/page/')
+        self.assertContains(
+            response,
+            '/static/mptt/draggable-admin.js',
+            1,
+        )
+        self.assertNotContains(
+            response,
+            '/static/content_editor/content_editor.js',
+        )
+
+        response = client.get('/admin/testapp/page/add/')
+        self.assertContains(
+            response,
+            '/static/content_editor/content_editor.js',
+            1,
+        )
+        self.assertNotContains(
+            response,
+            '/static/mptt/draggable-admin.js',
+        )
+
+    def test_add_empty_page(self):
+        client = self.login()
+
+        response = client.post(
+            '/admin/testapp/page/add/',
+            merge_dicts(
+                {
+                    'title': 'Home EN',
+                    'slug': 'home-en',
+                    'path': '/en/',
+                    'static_path': 1,
+                    'language_code': 'en',
+                    'application': '',
+                    'is_active': 1,
+                    'menu': 'main',
+                    'template_key': 'standard',
+                },
+                zero_management_form_data('testapp_richtext_set'),
+                zero_management_form_data('testapp_image_set'),
+                zero_management_form_data('testapp_snippet_set'),
+                zero_management_form_data('testapp_external_set'),
+            ),
+        )
+
+        self.assertRedirects(
+            response,
+            '/admin/testapp/page/',
+        )
+
+        page = Page.objects.get()
+        self.assertEqual(page.slug, 'home-en')
+        self.assertEqual(page.path, '/en/')  # static_path!
+
+        response = client.get(page.get_absolute_url())
+        self.assertContains(
+            response,
+            '<h1>Home EN</h1>',
+            1,
+        )
+
+    def test_add_page(self):
+        client = self.login()
+
+        response = client.post(
+            '/admin/testapp/page/add/',
+            merge_dicts(
+                {
+                    'title': 'Home EN',
+                    'slug': 'home-en',
+                    'path': '/en/',
+                    'static_path': 1,
+                    'language_code': 'en',
+                    'application': '',
+                    'is_active': 1,
+                    'menu': 'main',
+                    'template_key': 'standard',
+                },
+                zero_management_form_data('testapp_richtext_set'),
+                zero_management_form_data('testapp_image_set'),
+                zero_management_form_data('testapp_snippet_set'),
+                zero_management_form_data('testapp_external_set'),
+                {
+                    'testapp_richtext_set-TOTAL_FORMS': 1,
+                    'testapp_richtext_set-0-text': '<span style="font-weight:bold">Hello!</span>',  # noqa
+                    'testapp_richtext_set-0-region': 'main',
+                    'testapp_richtext_set-0-ordering': 10,
+                },
+            ),
+        )
+
+        self.assertRedirects(
+            response,
+            '/admin/testapp/page/',
+        )
+
+        page = Page.objects.get()
+        self.assertEqual(page.slug, 'home-en')
+        self.assertEqual(page.path, '/en/')  # static_path!
+
+        response = client.get(page.get_absolute_url())
+        self.assertContains(
+            response,
+            '<h1>Home EN</h1>',
+            1,
+        )
+        self.assertContains(
+            response,
+            '<strong>Hello!</strong>',  # HTML cleansing worked.
+            1,
+        )
+
+        # print(response, response.content.decode('utf-8'))
