b1709da568e7512968bdd32d77bd7772ca75a556
Matthias Kestenholz <mk@feinheit.ch>
2017-05-11 15:49:56 +0200

    Add some explanation to the feincms3_renderer template tag docs

--- feincms3/templatetags/feincms3_renderer.py
+++ feincms3/templatetags/feincms3_renderer.py
@@ -7,8 +7,11 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def render_plugin(context, plugin):
     """
-    Render a single plugin. See :mod:`feincms3.renderer` for additional
-    details.
+    Render a single plugin. Requires the
+    :class:`feincms3.renderer.TemplatePluginRenderer` instance in the context
+    as ``renderer``::
+
+        {% render_plugin plugin %}
 
     In general you should prefer
     :func:`~feincms3.templatetags.feincms3_renderer.render_region` over this
@@ -20,8 +23,15 @@ def render_plugin(context, plugin):
 @register.simple_tag(takes_context=True)
 def render_plugins(context, plugins):
     """
-    Render and concatenate a list of plugins. See
-    :mod:`feincms3.renderer` for additional details.
+    Render a list of plugins. Requires the
+    :class:`feincms3.renderer.TemplatePluginRenderer` instance in the context
+    as ``renderer``::
+
+        {% render_plugins plugins %}
+
+    This plugin is equivalent to::
+
+        {% for plugin in plugins %}{% render_plugin plugin %}{% endfor %}
 
     In general you should prefer
     :func:`~feincms3.templatetags.feincms3_renderer.render_region` over this
@@ -40,5 +50,17 @@ def render_region(context, regions, region, **kwargs):
     Render a single region. See :class:`~feincms3.renderer.Regions` for
     additional details. Any and all keyword arguments are forwarded to the
     ``render`` method of the ``Regions`` instance.
+
+    Usage::
+
+        {% render_region regions "main" %}
+
+    and::
+
+        {% render_region regions "main" timeout=15 %}
+
+    The main advantage of :class:`~feincms3.renderer.Regions` and
+    ``render_region`` is that regions only lazily load plugins when needed,
+    which means that caching also avoids the related database queries.
     """
     return regions.render(region, context, **kwargs)
