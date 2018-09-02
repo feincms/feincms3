82c4949f954bf659b2f6df5460ab41d0002d1644
Matthias Kestenholz <mk@feinheit.ch>
2018-09-02 15:40:53 +0200

    Report the location of the import instead of the module with the warning itself

--- feincms3/incubator/__init__.py
+++ feincms3/incubator/__init__.py
@@ -2,5 +2,7 @@ import warnings
 
 warnings.warn(
     "The feincms3.incubator module contains code which is subject to change"
-    " and/or removal without any prior warning."
+    " and/or removal without any prior warning.",
+    Warning,
+    stacklevel=2,
 )
