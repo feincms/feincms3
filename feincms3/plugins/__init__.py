f8b4b1a860b5c0a3ff16dbb8bbf83010bd9a1009
Matthias Kestenholz <mk@feinheit.ch>
2018-09-08 11:07:46 +0200

    feincms3.plugins: Stop hiding local import errors

--- feincms3/plugins/__init__.py
+++ feincms3/plugins/__init__.py
@@ -4,18 +4,26 @@ from . import html
 from . import snippet
 
 try:
+    import requests
+except ImportError:  # pragma: no cover
+    pass
+else:
     from . import external
+try:
+    import imagefield
 except ImportError:  # pragma: no cover
     pass
-try:
+else:
     from . import image
+try:
+    import feincms3.cleanse
 except ImportError:  # pragma: no cover
     pass
-try:
+else:
     from . import richtext
-except ImportError:  # pragma: no cover
-    pass
 try:
-    from . import versatileimage
+    import versatileimagefield
 except ImportError:  # pragma: no cover
     pass
+else:
+    from . import versatileimage
