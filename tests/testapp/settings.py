09672c7bb323f7909f1e38070634684a254dbeb5
Matthias Kestenholz <mk@feinheit.ch>
2019-12-05 22:52:09 +0100

    Fix the MEDIA_URL setting

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     "imagefield",
 ]
 
-MEDIA_ROOT = "/media/"
+MEDIA_URL = "/media/"
 STATIC_URL = "/static/"
 BASEDIR = os.path.dirname(__file__)
 MEDIA_ROOT = os.path.join(BASEDIR, "media/")
