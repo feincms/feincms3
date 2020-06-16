53efdbc5114fdcb36ee848fb460d924cdc7e82ed
Matthias Kestenholz <mk@feinheit.ch>
2020-06-16 14:13:32 +0200

    Remove some monkey patches

--- tests/manage.py
+++ tests/manage.py
@@ -4,21 +4,6 @@ import sys
 from os.path import abspath, dirname
 
 
-# Patch some stuff for Django 3.1 (because versatileimagefield isn't compatible yet)
-try:
-    from django.utils import six  # noqa
-except ImportError:
-    import six
-
-    sys.modules["django.utils.six"] = six
-
-try:
-    from django.utils.encoding import python_2_unicode_compatible  # noqa
-except ImportError:
-    from django.utils import encoding
-
-    encoding.python_2_unicode_compatible = lambda x: x
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testapp.settings")
 
