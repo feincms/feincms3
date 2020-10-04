aba501f18b163658d6eba8d379b6b3854b489d6a
Matthias Kestenholz <mk@feinheit.ch>
2020-10-04 11:54:20 +0200

    Remove a redundant value

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
     semi: ["error", "always"],
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "prettier/prettier": "error",
