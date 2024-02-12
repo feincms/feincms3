71601862e22596fe73483a81168e8ad36efa0058
Matthias Kestenholz <mk@feinheit.ch>
2024-02-12 17:59:33 +0100

    It's only about reversing here

--- tests/testapp/translated_articles_urls.py
+++ tests/testapp/translated_articles_urls.py
@@ -1,18 +1,5 @@
-from django.shortcuts import get_object_or_404
 from django.urls import path
-
-from feincms3.applications import page_for_app_request
-from feincms3.shortcuts import render_detail
-from testapp.models import TranslatedArticle
-
-
-def detail(request, pk):
-    page = page_for_app_request(request)
-    page.activate_language(request)
-    return render_detail(
-        request, get_object_or_404(TranslatedArticle, pk=pk), {"page": page}
-    )
 
 
 app_name = "translated-articles"
-urlpatterns = [path("<int:pk>/", detail, name="detail")]
+urlpatterns = [path("<int:pk>/", lambda request, pk: None, name="detail")]
